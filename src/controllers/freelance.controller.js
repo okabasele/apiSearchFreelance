0667210f777ef44231cf14aa9a7cb8170f9691fc
Océane Kabasele <ocekabasele@gmail.com>
2023-02-05 17:09:57 +0100

    post freelance

--- src/controllers/freelance.controller.js
+++ src/controllers/freelance.controller.js
@@ -1,13 +1,34 @@
 const Freelance = require("../models/freelance.model");
 const User = require("../models/user.model");
+const Job = require("../models/job.model");
+const Skill = require("../models/skill.model");
 
 exports.register = async (req, res, next) => {
   try {
+    //Verifier que l'utilisateur n'existe pas et que le job id et skills id existent
     const foundUser = await User.findOne({ email: req.body.email });
     if (foundUser) {
       return next(new Error("User already exists"));
     }
+    const foundJob = await Job.findById(req.body.job);
+    if (!foundJob) {
+      return next(new Error(`Job with id ${req.body.job} does not exist.`));
+    }
+    //Tableau de promesses
+    const skillsPromises = req.body.skills.map(async (skillId) => {
+      try {
+        const foundSkill = await Skill.findById(skillId);
+        if (!foundSkill) {
+          throw new Error(`Skill with id ${skillId} does not exist.`);
+        }
+        return foundSkill._id;
+      } catch (error) {
+        throw error;
+      }
+    });
 
+    const foundSkills = await Promise.all(skillsPromises);
+    //création de l'utilisateur
     const newUser = new User({
       firstname: req.body.firstname,
       lastname: req.body.lastname,
@@ -20,17 +41,18 @@ exports.register = async (req, res, next) => {
     });
     const newUserToSave = await newUser.save();
 
+    //création du freelance
     const newFreelance = new Freelance({
       dailyWage: req.body.dailyWage,
       yearsExp: req.body.yearsExp,
-      skills: req.body.skills,
-      job: req.body.job,
+      skills: foundSkills,
+      job: foundJob._id,
       user: newUserToSave._id,
     });
 
     const newFreelanceToSave = await newFreelance.save();
     return res.send(newFreelanceToSave);
   } catch (err) {
-   return next(err);
+    return next(err);
   }
 };
