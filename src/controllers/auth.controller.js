932aee2de763c3eeba6f3c09f9706b703c24a2b7
okabasele <ocekabasele@gmail.com>
2023-02-08 18:06:25 +0100

    auth

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -1,39 +1,55 @@
 const User = require("../models/user.model");
+const Company = require("../models/company.model");
 const bcrypt = require("bcrypt");
 var jwt = require("jsonwebtoken");
 
-exports.login = (req, res, next) => {
-  User.findOne({ email: req.body.email })
-    .then((user) => {
-      if (!user) {
-        // return res.status(404).send({ message: "User not found", });
-        return next(
-          new Error("User not found", {
-            statusCode: 404,
-          })
-        );
-      }
-      let passwordValid = bcrypt.compareSync(req.body.password, user.password);
-      if (!passwordValid) {
-        // return res.status(401).send({
-        //   message: "Password not valid",
-        //   auth: false,
-        // });
-        return next(
-          new Error("Password not valid", {
-            statusCode: 401,
-            auth: false,
-          })
-        );
-      }
-      let token = jwt.sign(
-        {
-          id: user.id,
-          isAdmin: user.isAdmin,
-        },
-        process.env.TOKEN_SECRET
+exports.login = async (req, res, next) => {
+  try {
+    //Recuperer l'utilisateur
+    const foundUser = await User.findOne({ email: req.body.email });
+    //Verifier s'il existe
+    if (!foundUser) {
+      return next(
+        new Error("User not found", {
+          statusCode: 404,
+        })
       );
-      res.send({ message: "User logged in", auth: true, userToken: token });
-    })
-    .catch((err) => next(err));
+    }
+
+    //Savoir si l'utilisateur est une entreprise ou un freelance
+    var role = "";
+    const foundCompany = await Company.findOne({ user: foundUser._id });
+    if (foundUser.isAdmin) {
+      role = "admin";
+    } else if (foundCompany) {
+      role = "company";
+    } else {
+      role = "freelance";
+    }
+
+    //Si l'utilisateur existe on déchiffre le mot de passe
+    let passwordValid = bcrypt.compareSync(req.body.password, foundUser.password);
+    if (!passwordValid) {
+      return next(
+        new Error("Password not valid", {
+          statusCode: 401,
+          auth: false,
+        })
+      );
+    }
+
+    //On crée le token
+    let token = jwt.sign(
+      {
+        id: foundUser._id,
+        role: role,
+        isAdmin: foundUser.isAdmin,
+      },
+      process.env.TOKEN_SECRET
+    );
+    res.send({ message: "User logged in", auth: true, userToken: token });
+  } catch (error) {
+    next(error);
+  }
+
 };
